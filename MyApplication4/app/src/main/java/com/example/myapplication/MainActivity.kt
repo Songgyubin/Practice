a063a0410f9ce38d13c18f178e096ae4629681f9
DESKTOP-UD2063R\thd04 <thd0427__@naver.com>
2020-10-30 00:10:28 +0900

    progressbar

--- MyApplication4/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication4/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.myapplication
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.ext.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        progressDialog = ProgressDialog(this)
+        progressDialog.window!!.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
+
         val btnSignUp = binding.btnSignUp
+
         binding.edEmail.checkEmail(btnSignUp)
         binding.edPw.checkPw(btnSignUp)
         binding.edPwCheck.checkPwCheck(binding.edPw,btnSignUp)
@@ -23,7 +29,21 @@ class MainActivity : AppCompatActivity() {
         binding.rgGender.checkGender(btnSignUp)
         binding.cbService.isServiceChecked(btnSignUp)
 
-        btnSignUp.setOnClickListener { Toast.makeText(this,"회원가입",Toast.LENGTH_LONG).show() }
+        progressDialog.setCancelable(false)
+        btnSignUp.setOnClickListener { progressDialog.show()
+        Thread(Runnable {
+            run {
+                val task = object: TimerTask(){
+                    override fun run() {
+                        progressDialog.dismiss()
+                    }
+                }
+                Timer().let { it.schedule(task,2000) }
+            }
+        }).start()
+        }
+
+
 
     }
 }
---
+++ MyApplication4/app/src/main/java/com/example/myapplication/ProgressDialog.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+import android.app.Dialog
+import android.content.Context
+import android.view.Window
+
+
+class ProgressDialog(context: Context?) : Dialog(context!!) {
+    init {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.dialog_progress)
+    }
+
+}
