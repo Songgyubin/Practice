c40d51f98252daa09ccfed3c184b5f3bb0d0bbcd
DESKTOP-UD2063R\thd04 <thd0427__@naver.com>
2020-11-04 23:59:13 +0900

    사용자 정보

--- app/src/main/java/com/practice/signupmvc/InfoActivity.kt
+++ app/src/main/java/com/practice/signupmvc/InfoActivity.kt
@@ -1,12 +1,8 @@
 package com.practice.signupmvc
 
 import android.os.Bundle
-import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.practice.signupmvc.databinding.ActivityInfoBinding
 import kotlin.concurrent.thread
 
